58f3757df191d34547566b6f54eec4940b1bbe64
Julio Villalobos <julio.villalobos@softtek.com>
2020-03-18 10:16:01 -0600

    adService built

---
+++ adService/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using adService.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace adService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("adDB"));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo { Title = "My API", Version = "V1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+
+            //Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            //Enable middleware to serve swager-ui (HTML, JS, CSS, etc.),
+            //specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                //c.SwaggerEndpoint("../swagger/v1/swagger.json", "MyAPI V1");
+                //c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/V1/swagger.json", "MyAPI V1");
+            });
+
+
+            //Data de prueba si no hay en la base de datos
+            if (!context.Ads.Any())
+            {
+                context.Ads.AddRange(new List<Ad>()
+                {
+                    new Ad(){ Description = "¡¡¡Compra Videojuegos YA!!!" },
+                    new Ad(){ Description = "¡¡¡Compra Jerseys de Futbol YA!!!" },
+                    new Ad(){ Description = "¡¡¡Compra Cámaras de Vídeo YA!!!" },
+                    new Ad(){ Description = "¡¡¡Compra Proyectores YA!!!" },
+                    new Ad(){ Description = "¡¡¡Compra Pantallas plasma YA!!!" }
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
